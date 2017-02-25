dbf0cef281c02b5f500abaec3fccd264fea8cb2f
Trevor Sherwood <sherwoodwt@gmail.com>
2017-02-24 19:56:58 -0800

    delete now works, but with no form

--- bandsite/bandmanagement/urls.py
+++ bandsite/bandmanagement/urls.py
@@ -4,10 +4,12 @@ from bandmanagement.views import (
     list_bands,
     create_band,
     view_band,
+    delete_band,
 )
 
 urlpatterns = [
     url(r'^$', list_bands, name='listBands'),
     url(r'^create/$', create_band, name='createBand'),
     url(r'^(?P<band_id>[0-9]+)/$', view_band, name='viewBand'),
+    url(r'^(?P<band_id>[0-9]+)/delete/$', delete_band, name='deleteBand')
 ]
--- bandsite/bandmanagement/views.py
+++ bandsite/bandmanagement/views.py
@@ -1,11 +1,14 @@
 '''views for band management'''
 from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from bandmanagement.models import Band
 from bandmanagement.forms import BandForm
 
 # Create your views here.
+@login_required
 def list_bands(request):
     '''will kill this eventually'''
     bands = Band.objects.all()
@@ -14,6 +17,7 @@ def list_bands(request):
     }
     return render(request, 'list_page.html', context)
 
+@login_required
 def create_band(request):
     '''create a band model'''
     form = BandForm(request.POST or None)
@@ -21,17 +25,20 @@ def create_band(request):
         instance = form.save(commit=False)
         instance.created_by = request.user
         instance.save()
+        return HttpResponseRedirect(reverse('listBands'))
     context = {
         'form': form
     }
     return render(request, 'create.html', context)
 
+@login_required
 def edit_band(request):
-    '''create a band model'''
+    '''edit a band model'''
     return HttpResponse('I am alive')
 
+@login_required
 def view_band(request, band_id):
-    '''create a band model'''
+    '''view a specific a band model'''
     band = get_object_or_404(Band, pk=band_id)
     members = band.member_set.all()
     context = {
@@ -40,6 +47,9 @@ def view_band(request, band_id):
     }
     return render(request, 'detail_page.html', context)
 
-def delete_band(request):
-    '''create a band model'''
-    return HttpResponse('I am alive')
+@login_required
+def delete_band(request, band_id):
+    '''delete a band model'''
+    band = get_object_or_404(Band, pk=band_id)
+    band.delete()
+    return HttpResponseRedirect(reverse('listBands'))
